27f51a20f753c298220483c523dc757da28506a3
rudeigerc <rudeigerc@gmail.com>
2017-12-17 00:49:44 +0800

    add lab3

---
+++ lab3/inode_manager.cc
@@ -0,0 +1,402 @@
+#include "inode_manager.h"
+
+// disk layer -----------------------------------------
+
+disk::disk()
+{
+  	bzero(blocks, sizeof(blocks));
+}
+
+void disk::read_block(blockid_t id, char *buf)
+{
+	/*
+	*your lab1 code goes here.
+	*if id is smaller than 0 or larger than BLOCK_NUM
+	*or buf is null, just return.
+	*put the content of target block into buf.
+	*hint: use memcpy
+	*/
+	if (id < 0 || id > BLOCK_NUM || buf == NULL) return;
+	memcpy(buf, blocks[id], BLOCK_SIZE);
+}
+
+void disk::write_block(blockid_t id, const char *buf)
+{
+	/*
+	*your lab1 code goes here.
+	*hint: just like read_block
+	*/
+	if (id < 0 || id > BLOCK_NUM || buf == NULL) return;
+	memcpy(blocks[id], buf, BLOCK_SIZE);
+}
+
+// block layer -----------------------------------------
+
+// Allocate a free disk block.
+blockid_t block_manager::alloc_block()
+{
+	/*
+	* your lab1 code goes here.
+	* note: you should mark the corresponding bit in block bitmap when alloc.
+	* you need to think about which block you can start to be allocated.
+
+	* hint: use macro IBLOCK and BBLOCK.
+			use bit operation.
+			remind yourself of the layout of disk.
+	*/
+	blockid_t id = IBLOCK(INODE_NUM, BLOCK_NUM);
+	char tmp[BLOCK_SIZE];
+	while (id < BLOCK_NUM) {
+		d->read_block(BBLOCK(id), tmp);
+		uint32_t num = id % BLOCK_SIZE;
+		uint32_t* buf = &((uint32_t *)tmp)[num/8];
+		if (!(*buf & (1 << num))) {
+			*buf |= (1 << num);
+			d->write_block(BBLOCK(id), tmp);
+			break;
+		}
+		id++;
+	}
+	return id;
+}
+
+void block_manager::free_block(uint32_t id)
+{
+	/*
+	* your lab1 code goes here.
+	* note: you should unmark the corresponding bit in the block bitmap when free.
+	*/
+    char tmp[BLOCK_SIZE];
+    d->read_block(BBLOCK(id), tmp);
+    uint32_t num = id % BLOCK_SIZE;
+    uint32_t* buf = &((uint32_t *)tmp)[num/8];
+    *buf &= ~(1 << num);
+    d->write_block(BBLOCK(id), tmp);
+}
+
+// The layout of disk should be like this:
+// |<-sb->|<-free block bitmap->|<-inode table->|<-data->|
+block_manager::block_manager()
+{
+	d = new disk();
+
+	// format the disk
+	sb.size = BLOCK_SIZE * BLOCK_NUM;
+	sb.nblocks = BLOCK_NUM;
+	sb.ninodes = INODE_NUM;
+
+}
+
+void block_manager::read_block(uint32_t id, char *buf)
+{
+	d->read_block(id, buf);
+}
+
+void block_manager::write_block(uint32_t id, const char *buf)
+{
+	d->write_block(id, buf);
+}
+
+// inode layer -----------------------------------------
+
+inode_manager::inode_manager()
+{
+	bm = new block_manager();
+	uint32_t root_dir = alloc_inode(extent_protocol::T_DIR);
+	if (root_dir != 1) {
+		printf("\tim: error! alloc first inode %d, should be 1\n", root_dir);
+		exit(0);
+	}
+}
+
+/* Create a new file.
+ * Return its inum. */
+uint32_t inode_manager::alloc_inode(uint32_t type)
+{
+	/*
+	* your lab1 code goes here.
+	* note: the normal inode block should begin from the 2nd inode block.
+	* the 1st is used for root_dir, see inode_manager::inode_manager().
+
+	* if you get some heap memory, do not forget to free it.
+	*/
+	uint32_t inum = 1;
+	while (inum < INODE_NUM) {
+		struct inode *ino = get_inode(inum);
+		if (ino) {
+			free(ino);
+			inum++;
+		} else {
+			struct inode buf;
+			buf.type = type;
+			buf.atime = time(0);
+			buf.mtime = time(0);
+			buf.ctime = time(0);
+			buf.size = 0;
+			put_inode(inum, &buf);
+			return inum;
+		}
+	}
+	return 0;
+}
+
+void inode_manager::free_inode(uint32_t inum)
+{
+	/*
+	* your lab1 code goes here.
+	* note: you need to check if the inode is already a freed one;
+	* if not, clear it, and remember to write back to disk.
+	* do not forget to free memory if necessary.
+	*/
+	struct inode *ino = get_inode(inum);
+	if (ino) {
+		ino->type = 0;
+		ino->atime = time(0);
+		ino->mtime = time(0);
+		ino->size = 0;
+		put_inode(inum, ino);
+	}
+	free(ino);
+}
+
+
+/* Return an inode structure by inum, NULL otherwise.
+ * Caller should release the memory. */
+struct inode* inode_manager::get_inode(uint32_t inum)
+{
+	struct inode *ino, *ino_disk;
+	char buf[BLOCK_SIZE];
+
+	printf("\tim: get_inode %d\n", inum);
+
+	if (inum < 0 || inum >= INODE_NUM) {
+		printf("\tim: inum out of range\n");
+		return NULL;
+	}
+
+	bm->read_block(IBLOCK(inum, bm->sb.nblocks), buf);
+	// printf("%s:%d\n", __FILE__, __LINE__);
+
+	ino_disk = (struct inode*)buf + inum%IPB;
+	if (ino_disk->type == 0) {
+		printf("\tim: inode not exist\n");
+		return NULL;
+	}
+
+	ino = (struct inode*)malloc(sizeof(struct inode));
+	*ino = *ino_disk;
+
+	return ino;
+}
+
+void inode_manager::put_inode(uint32_t inum, struct inode *ino)
+{
+	char buf[BLOCK_SIZE];
+	struct inode *ino_disk;
+
+	printf("\tim: put_inode %d\n", inum);
+	if (ino == NULL)
+		return;
+
+	bm->read_block(IBLOCK(inum, bm->sb.nblocks), buf);
+	ino_disk = (struct inode*)buf + inum%IPB;
+	*ino_disk = *ino;
+	bm->write_block(IBLOCK(inum, bm->sb.nblocks), buf);
+}
+
+#define MIN(a,b) ((a)<(b) ? (a) : (b))
+
+/* Get all the data of a file by inum.
+ * Return alloced data, should be freed by caller. */
+void inode_manager::read_file(uint32_t inum, char **buf_out, int *size)
+{
+	/*
+	* your lab1 code goes here.
+	* note: read blocks related to inode number inum,
+	* and copy them to buf_out
+	*/
+	struct inode *ino = get_inode(inum);
+	if (!ino) return;
+    ino->atime = time(0);
+	*size = ino->size;
+	uint32_t nblocks = 0;
+
+	// nblocks = ⌈(*size) / BLOCK_SIZE⌉
+	if (*size) nblocks = (*size) / BLOCK_SIZE + !((*size) % BLOCK_SIZE == 0);
+	*buf_out = (char *)malloc(nblocks * BLOCK_SIZE);
+
+	for (uint32_t i = 0; i < MIN(nblocks, NDIRECT); i++) {
+		bm->read_block(ino->blocks[i], *buf_out + i * BLOCK_SIZE);
+	}
+
+	if (nblocks > NDIRECT) {
+		blockid_t tmp[NINDIRECT];
+		bm->read_block(ino->blocks[NDIRECT], (char *)tmp);
+		for (uint32_t i = 0; i < nblocks - NDIRECT; i++) {
+			bm->read_block(tmp[i], *buf_out + NDIRECT * BLOCK_SIZE + i * BLOCK_SIZE);
+		}
+	}
+	free(ino);
+}
+
+/* alloc/free blocks if needed */
+void inode_manager::write_file(uint32_t inum, const char *buf, int size)
+{
+	/*
+	* your lab1 code goes here.
+	* note: write buf to blocks of inode inum.
+	* you need to consider the situation when the size of buf
+	* is larger or smaller than the size of original inode.
+	* you should free some blocks if necessary.
+	*/
+	struct inode *ino = get_inode(inum);
+	if (!ino) return;
+
+	uint32_t noldBlocks = 0;
+	uint32_t nnewBlocks = 0;
+
+	// noldBlocks = ⌈(ino->size) / BLOCK_SIZE⌉
+	if (ino->size) noldBlocks = (ino->size) / BLOCK_SIZE + !((ino->size) % BLOCK_SIZE == 0);
+	// nnewBlocks = ⌈size / BLOCK_SIZE⌉
+	if (size) nnewBlocks = size / BLOCK_SIZE + !(size % BLOCK_SIZE == 0);
+
+	if (nnewBlocks <= noldBlocks) {
+		// nnewBlocks <= noldBlocks
+		// free useless blocks
+		if (noldBlocks <= NDIRECT) {
+			// nnewBlocks <= noldBlocks <= NDIRECT
+			for (uint32_t i = nnewBlocks; i < noldBlocks; i++) {
+				bm->free_block(ino->blocks[i]);
+			}
+		} else if (nnewBlocks > NDIRECT) {
+			// NDIRECT < nnewBlocks <= noldBlocks
+			blockid_t tmp[NINDIRECT];
+			bm->read_block(ino->blocks[NDIRECT], (char *)tmp);
+			for (uint32_t i = nnewBlocks; i < noldBlocks; i++) {
+				bm->free_block(tmp[i-NDIRECT]);
+			}
+		} else {
+			// nnewBlocks < NDIRECT < noldBlocks
+			for (uint32_t i = nnewBlocks; i < NDIRECT; i++) {
+				bm->free_block(ino->blocks[i]);
+			}
+			blockid_t tmp[NINDIRECT];
+			bm->read_block(ino->blocks[NDIRECT], (char *)tmp);
+			for (uint32_t i = 0; i < noldBlocks - NDIRECT; i++) {
+				bm->free_block(tmp[i]);
+			}
+			bm->free_block(ino->blocks[NDIRECT]);
+		}
+	} else {
+		// nnewBlocks > noldBlocks
+		// alloc new blocks
+		if (nnewBlocks <= NDIRECT) {
+			// noldBlocks <= nnewBlocks <= NDIRECT
+			for (uint32_t i = noldBlocks; i < nnewBlocks; i++) {
+				ino->blocks[i] = bm->alloc_block();
+			}
+		} else if (noldBlocks > NDIRECT) {
+			// NDIRECT < noldBlocks <= nnewBlocks
+			blockid_t tmp[NINDIRECT];
+			bm->read_block(ino->blocks[NDIRECT], (char *)tmp);
+			for (uint32_t i = noldBlocks; i < nnewBlocks; i++) {
+				tmp[i-NDIRECT] = bm->alloc_block();
+			}
+			bm->write_block(ino->blocks[NDIRECT], (char *)tmp);
+		} else {
+			// noldBlocks < NDIRECT < nnewBlocks
+			for (uint32_t i = noldBlocks; i < NDIRECT; i++) {
+				ino->blocks[i] = bm->alloc_block();
+			}
+			blockid_t tmp[NINDIRECT];
+			ino->blocks[NDIRECT] = bm->alloc_block();
+			for (uint32_t i = 0; i < nnewBlocks - NDIRECT; i++) {
+				tmp[i] = bm->alloc_block();
+			}
+			bm->write_block(ino->blocks[NDIRECT], (char *)tmp);
+		}
+	}
+
+    char tmp[BLOCK_SIZE];
+    char _tmp[BLOCK_SIZE];
+    uint32_t ptr = 0;
+    for (uint32_t i = 0; i < NDIRECT && ptr < size; i++) {
+        if (size - ptr > BLOCK_SIZE) {
+            bm->write_block(ino->blocks[i], buf + ptr);
+            ptr += BLOCK_SIZE;
+        } else {
+            uint32_t len = size - ptr;
+            memcpy(tmp, buf + ptr, len);
+            bm->write_block(ino->blocks[i], tmp);
+            ptr += len;
+        }
+    }
+    if (ptr < size) {
+        bm->read_block(ino->blocks[NDIRECT], _tmp);
+        for (uint32_t i = 0; i < NINDIRECT && ptr < size; i++) {
+            blockid_t id = *((blockid_t *)_tmp + i);
+            if (size - ptr > BLOCK_SIZE) {
+                bm->write_block(id, buf + ptr);
+                ptr += BLOCK_SIZE;
+            } else {
+                uint32_t len = size - ptr;
+                memcpy(tmp, buf + ptr, len);
+                bm->write_block(id, tmp);
+                ptr += len;
+            }
+        }
+    }
+
+    ino->size = size;
+    ino->mtime = time(0);
+    ino->ctime = time(0);
+	put_inode(inum, ino);
+	free(ino);
+}
+
+void inode_manager::getattr(uint32_t inum, extent_protocol::attr &a)
+{
+	/*
+	* your lab1 code goes here.
+	* note: get the attributes of inode inum.
+	* you can refer to "struct attr" in extent_protocol.h
+	*/
+	struct inode *ino = get_inode(inum);
+	if (ino) {
+		a.type = ino->type;
+		a.atime = ino->atime;
+		a.mtime = ino->mtime;
+		a.ctime = ino->ctime;
+		a.size = ino->size;
+        free(ino);
+	}
+}
+
+void inode_manager::remove_file(uint32_t inum)
+{
+	/*
+	* your lab1 code goes here
+	* note: you need to consider about both the data block and inode of the file
+	* do not forget to free memory if necessary.
+	*/
+	struct inode *ino = get_inode(inum);
+	uint32_t nblocks = 0;
+
+	// nblocks = ⌈(ino->size) / BLOCK_SIZE⌉
+	if (ino->size) nblocks = (ino->size) / BLOCK_SIZE + !((ino->size) % BLOCK_SIZE == 0);
+
+	for (uint32_t i = 0; i < MIN(nblocks, NDIRECT); i++){
+		bm->free_block(ino->blocks[i]);
+	}
+
+	if (nblocks > NDIRECT) {
+		blockid_t tmp[NINDIRECT];
+		bm->read_block(ino->blocks[NDIRECT], (char *)tmp);
+		for (uint32_t i = 0; i < nblocks - NDIRECT; i++) {
+			bm->free_block(tmp[i]);
+		}
+		bm->free_block(ino->blocks[NDIRECT]);
+	}
+	free_inode(inum);
+	free(ino);
+}
---
+++ lab3/yfs_client.cc
@@ -0,0 +1,444 @@
+// yfs client.  implements FS operations using extent and lock server
+#include "yfs_client.h"
+#include "extent_client.h"
+#include <sstream>
+#include <iostream>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+yfs_client::yfs_client(std::string extent_dst)
+{
+    ec = new extent_client(extent_dst);
+    if (ec->put(1, "") != extent_protocol::OK)
+        printf("error init root dir\n"); // XYB: init root dir
+}
+
+yfs_client::inum yfs_client::n2i(std::string n)
+{
+    std::istringstream ist(n);
+    unsigned long long finum;
+    ist >> finum;
+    return finum;
+}
+
+std::string yfs_client::filename(inum inum)
+{
+    std::ostringstream ost;
+    ost << inum;
+    return ost.str();
+}
+
+bool yfs_client::isfile(inum inum)
+{
+    extent_protocol::attr a;
+
+    if (ec->getattr(inum, a) != extent_protocol::OK) {
+        printf("error getting attr\n");
+        return false;
+    }
+
+    if (a.type == extent_protocol::T_FILE) {
+        printf("isfile: %lld is a file\n", inum);
+        return true;
+    }
+    printf("isfile: %lld is not a file\n", inum);
+    return false;
+}
+
+/* Your code here for Lab...
+ * You may need to add routines such as
+ * readlink, issymlink here to implement symbolic link.
+ *
+ */
+
+bool yfs_client::issymlink(inum inum) {
+    extent_protocol::attr a;
+
+    if (ec->getattr(inum, a) != extent_protocol::OK) {
+        printf("error getting attr\n");
+        return false;
+    }
+
+    if (a.type == extent_protocol::T_SYMLINK) {
+        printf("issymlink: %lld is a symlink\n", inum);
+        return true;
+    }
+    printf("issymlink: %lld is not a symlink\n", inum);
+    return false;
+}
+
+int yfs_client::getsymlink(inum inum, symlinkinfo &sin) {
+    int r = OK;
+
+    printf("getsymlink %016llx\n", inum);
+    extent_protocol::attr a;
+    if (ec->getattr(inum, a) != extent_protocol::OK) {
+        r = IOERR;
+        goto release;
+    }
+
+    sin.atime = a.atime;
+    sin.mtime = a.mtime;
+    sin.ctime = a.ctime;
+    sin.size = a.size;
+    printf("getsymlink %016llx -> sz %llu\n", inum, sin.size);
+
+release:
+    return r;
+}
+
+int yfs_client::symlink(const char *link, inum parent, const char *name, inum &ino_out) {
+    int r = OK;
+    bool found = false;
+    std::string buf;
+
+    if (!isdir(parent)) return IOERR;
+    lookup(parent, name, found, ino_out);
+    if (found) return EXIST;
+
+    if (ec->create(extent_protocol::T_SYMLINK, ino_out) != extent_protocol::OK) {
+        printf("symlink - create error\n");
+        return IOERR;
+    }
+    if (ec->put(ino_out, std::string(link)) != extent_protocol::OK) {
+        printf("symlink - put error\n");
+        return IOERR;
+    }
+    if (ec->get(parent, buf) != extent_protocol::OK) {
+        printf("symlink - get error\n");
+        return IOERR;
+    }
+    buf += std::string(name) + "//" + filename(ino_out) + "//";
+    if (ec->put(parent, buf) != extent_protocol::OK) {
+        printf("symlink - put error\n");
+        return IOERR;
+    }
+    return r;
+}
+
+int yfs_client::readlink(inum ino, std::string &link) {
+    int r = OK;
+    if (!issymlink(ino)) return IOERR;
+    if (ec->get(ino, link) != extent_protocol::OK) {
+        printf("readlink - get error\n");
+        return IOERR;
+    }
+    return r;
+}
+
+
+bool yfs_client::isdir(inum inum)
+{
+    // Oops! is this still correct when you implement symlink?
+    extent_protocol::attr a;
+
+    if (ec->getattr(inum, a) != extent_protocol::OK) {
+        printf("error getting attr\n");
+        return false;
+    }
+
+    if (a.type == extent_protocol::T_DIR) {
+        printf("isdir: %lld is a directory\n", inum);
+        return true;
+    }
+    printf("isdir: %lld is not a directory\n", inum);
+    return false;
+}
+
+int yfs_client::getfile(inum inum, fileinfo &fin)
+{
+    int r = OK;
+
+    printf("getfile %016llx\n", inum);
+    extent_protocol::attr a;
+    if (ec->getattr(inum, a) != extent_protocol::OK) {
+        r = IOERR;
+        goto release;
+    }
+
+    fin.atime = a.atime;
+    fin.mtime = a.mtime;
+    fin.ctime = a.ctime;
+    fin.size = a.size;
+    printf("getfile %016llx -> sz %llu\n", inum, fin.size);
+
+release:
+    return r;
+}
+
+int yfs_client::getdir(inum inum, dirinfo &din)
+{
+    int r = OK;
+
+    printf("getdir %016llx\n", inum);
+    extent_protocol::attr a;
+    if (ec->getattr(inum, a) != extent_protocol::OK) {
+        r = IOERR;
+        goto release;
+    }
+    din.atime = a.atime;
+    din.mtime = a.mtime;
+    din.ctime = a.ctime;
+
+release:
+    return r;
+}
+
+
+#define EXT_RPC(xx) do { \
+    if ((xx) != extent_protocol::OK) { \
+        printf("EXT_RPC Error: %s:%d \n", __FILE__, __LINE__); \
+        r = IOERR; \
+        goto release; \
+    } \
+} while (0)
+
+// Only support set size of attr
+int yfs_client::setattr(inum ino, size_t size)
+{
+    int r = OK;
+
+    /*
+     * your lab2 code goes here.
+     * note: get the content of inode ino, and modify its content
+     * according to the size (<, =, or >) content length.
+     */
+
+    extent_protocol::attr a;
+    std::string buf;
+
+    if (ec->getattr(ino, a) != extent_protocol::OK) {
+        printf("setattr - getattr error\n");
+        return IOERR;
+    }
+    if (ec->get(ino, buf) != extent_protocol::OK) {
+        printf("setattr - get error\n");
+        return IOERR;
+    }
+    if (buf.length() > size) {
+        buf.erase(size);
+    } else {
+        buf.resize(size);
+    }
+    if (ec->put(ino, buf) != extent_protocol::OK) {
+        printf("setattr - put error\n");
+        return IOERR;
+    }
+    return r;
+}
+
+int yfs_client::create(inum parent, const char *name, mode_t mode, inum &ino_out)
+{
+    int r = OK;
+
+    /*
+     * your lab2 code goes here.
+     * note: lookup is what you need to check if file exist;
+     * after create file or dir, you must remember to modify the parent infomation.
+     */
+
+    bool found = false;
+    std::string buf;
+
+    if (!isdir(parent)) return IOERR;
+    lookup(parent, name, found, ino_out);
+    if (found) return EXIST;
+
+    if (ec->create(extent_protocol::T_FILE, ino_out) != extent_protocol::OK) {
+        printf("create - create error\n");
+        return IOERR;
+    }
+    if (ec->get(parent, buf) != extent_protocol::OK) {
+        printf("create - get error\n");
+        return IOERR;
+    }
+    buf += std::string(name) + "//" + filename(ino_out) + "//";
+    printf("**********CRAETEDEBUG**********\n%s\n**********ENDOFCREATEDEBUG**********\n", buf.c_str());   //TODO
+    if (ec->put(parent, buf) != extent_protocol::OK) {
+        printf("create - put error\n");
+        return IOERR;
+    }
+    return r;
+}
+
+int yfs_client::mkdir(inum parent, const char *name, mode_t mode, inum &ino_out)
+{
+    int r = OK;
+
+    /*
+     * your lab2 code goes here.
+     * note: lookup is what you need to check if directory exist;
+     * after create file or dir, you must remember to modify the parent infomation.
+     */
+
+    bool found = false;
+    std::string buf;
+
+    if (!isdir(parent)) return IOERR;
+    lookup(parent, name, found, ino_out);
+    if (found) return EXIST;
+
+    if (ec->create(extent_protocol::T_DIR, ino_out) != extent_protocol::OK) {
+        printf("mkdir - create error\n");
+        return IOERR;
+    }
+    if (ec->get(parent, buf) != extent_protocol::OK) {
+        printf("mkdir - get error\n");
+        return IOERR;
+    }
+    buf += std::string(name) + "//" + filename(ino_out) + "//";
+    if (ec->put(parent, buf) != extent_protocol::OK) {
+        printf("mkdir - put error\n");
+        return IOERR;
+    }
+    return r;
+}
+
+int yfs_client::lookup(inum parent, const char *name, bool &found, inum &ino_out)
+{
+    int r = OK;
+
+    /*
+     * your lab2 code goes here.
+     * note: lookup file from parent dir according to name;
+     * you should design the format of directory content.
+     */
+
+    found = false;
+    std::string buf;
+    std::list<dirent> dirent_list;
+
+    if (!isdir(parent)) return IOERR;
+    if (ec->get(parent, buf) != extent_protocol::OK) {
+        printf("lookup - get error\n");
+        return IOERR;
+    }
+    printf("**********LOOKUPDEBUG**********\n%s\n**********ENDOFLOOKUPDEBUG**********\n", buf.c_str());   //TODO
+    readdir(parent, dirent_list);
+    for (std::list<dirent>::iterator it = dirent_list.begin(); it != dirent_list.end(); it++) {
+        if (std::string(name) == it->name){
+            found = true;
+            ino_out = it->inum;
+            break;
+        }
+    }
+
+    return r;
+}
+
+int yfs_client::readdir(inum dir, std::list<dirent> &list)
+{
+    int r = OK;
+
+    /*
+     * your lab2 code goes here.
+     * note: you should parse the dirctory content using your defined format,
+     * and push the dirents to the list.
+     */
+
+    std::string buf;
+
+    if (!isdir(dir)) return IOERR;
+    if (ec->get(dir, buf) != extent_protocol::OK) {
+        printf("readdir - get error\n");
+        return IOERR;
+    }
+    uint32_t ptr = 0;
+    uint32_t size = buf.length();
+    while (ptr < size) {
+        dirent dirents;
+        uint32_t tmp = buf.find("//", ptr);
+        dirents.name = buf.substr(ptr, tmp - ptr);
+        ptr = tmp + 2;
+        tmp = buf.find("//", ptr);
+        dirents.inum = n2i(buf.substr(ptr, tmp - ptr));
+        ptr = tmp + 2;
+        list.push_back(dirents);
+    }
+    return r;
+}
+
+int yfs_client::read(inum ino, size_t size, off_t off, std::string &data)
+{
+    int r = OK;
+
+    /*
+     * your lab2 code goes here.
+     * note: read using ec->get().
+     */
+
+    std::string buf;
+    if (ec->get(ino, buf) != extent_protocol::OK) {
+        printf("read - get error\n");
+        return IOERR;
+    }
+    data = off > (off_t)buf.length() ? "" : buf.substr(off, size);
+    return r;
+}
+
+int yfs_client::write(inum ino, size_t size, off_t off, const char *data, size_t &bytes_written)
+{
+    int r = OK;
+
+    /*
+     * your lab2 code goes here.
+     * note: write using ec->put().
+     * when off > length of original file, fill the holes with '\0'.
+     */
+
+    std::string buf;
+    if (ec->get(ino, buf) != extent_protocol::OK) {
+        printf("write - get error\n");
+        return IOERR;
+    }
+    size_t tmp = (size_t)(size + off);
+    if (tmp > buf.length()) buf.resize(tmp, '\0');
+    for (size_t i = off; i < tmp; i++) {
+        buf[i] = data[i-off];
+    }
+    if (ec->put(ino, buf) != extent_protocol::OK) {
+        printf("write - put error\n");
+        return IOERR;
+    }
+    return r;
+}
+
+int yfs_client::unlink(inum parent, const char *name)
+{
+    int r = OK;
+
+    /*
+     * your lab2 code goes here.
+     * note: you should remove the file using ec->remove,
+     * and update the parent directory content.
+     */
+
+    bool found = false;
+    inum ino;
+    std::list<dirent> dirent_list;
+    std::string buf;
+
+    if (!isdir(parent)) return IOERR;
+    lookup(parent, name, found, ino);
+    if (found) {
+        ec->remove(ino);
+        readdir(parent, dirent_list);
+        for (std::list<dirent>::iterator it = dirent_list.begin(); it != dirent_list.end(); it++) {
+            if (std::string(name) == it->name){
+                dirent_list.erase(it);
+                break;
+            }
+        }
+    }
+    for (std::list<dirent>::iterator it = dirent_list.begin(); it != dirent_list.end(); it++) {
+        buf += std::string(it->name) + "//" + filename(it->inum) + "//";
+    }
+    if (ec->put(parent, buf) != extent_protocol::OK) {
+        printf("unlink - put error\n");
+        return IOERR;
+    }
+    return r;
+}
